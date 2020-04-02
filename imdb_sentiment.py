5a48d2b3224ad95a2dfe2940a791c6f355b8229c
emarkou <eleni.mark@yahoo.com>
2020-04-02 12:01:53 +0300

    feat: Create project skeleton
    Add the definition of the basic functions that are going to be used in the project.

---
+++ imdb_sentiment.py
@@ -0,0 +1,23 @@
+def read_imdb_data(dir):
+    raise NotImplementedError
+
+def prepare_data():
+    raise NotImplementedError
+
+def review_word_tokenizer():
+    raise NotImplementedError
+
+def preprocess_data():
+    raise NotImplementedError
+
+def extract_bow_features():
+    raise NotImplementedError
+
+def classify_gboost():
+    raise NotImplementedError
+
+def main():
+    raise NotImplementedError
+
+if __name__ == "__main__":
+    main()
