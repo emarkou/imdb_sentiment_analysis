a50918976853e16f704bb17da79d27f8721b595f
emarkou <eleni.mark@yahoo.com>
2020-04-02 13:19:18 +0300

    feat: Add data folder, implement read_imdb_data
    Add data folder containing all necessary idb files. Implement read_imdb_data() function which reads thw review files and produces nested dictionaries containing reviews' text and corresponding labels.

--- imdb_sentiment.py
+++ imdb_sentiment.py
@@ -1,5 +1,39 @@
-def read_imdb_data(dir):
-    raise NotImplementedError
+import os
+import glob
+
+def read_imdb_data(data_dir):
+    """
+    Read review data from specified directory and return reviews and labels(pos/neg) as nested dictionaries
+    :param data_dir: the directory where data reside.
+                     Assumed directory structure:
+                     - data/
+                         - train/
+                             - pos/
+                             - neg/
+                         - test/
+                             - pos/
+                             - neg/
+    :return: nested dictionaries containing data and reviews
+    """
+    data = {}
+    labels = {}
+
+    for data_type in ['train', 'test']:
+        data[data_type] = {}
+        labels[data_type] = {}
+
+        for sentiment in ['pos', 'neg']:
+            data[data_type][sentiment] = []
+            labels[data_type][sentiment] = []
+
+            path = os.path.join(data_dir, data_type, sentiment, '*.txt')
+            files = glob.glob(path)
+
+            for f in files:
+                with open(f) as review:
+                    data[data_type][sentiment].append(review.read())
+                    labels[data_type][sentiment].append(sentiment)
+    return data, labels
 
 def prepare_data():
     raise NotImplementedError
@@ -17,7 +51,7 @@ def classify_gboost():
     raise NotImplementedError
 
 def main():
-    raise NotImplementedError
+    data, labels = read_imdb_data('data')
 
 if __name__ == "__main__":
     main()
