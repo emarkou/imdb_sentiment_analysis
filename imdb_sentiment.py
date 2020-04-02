2b6dfca8c6e0072d27b0ed81a4c8b6e7784b600f
emarkou <eleni.mark@yahoo.com>
2020-04-02 16:34:20 +0300

    feat: Implement plot_wordcloud, prepare_data,review_to_words

--- imdb_sentiment.py
+++ imdb_sentiment.py
@@ -1,5 +1,26 @@
 import os
 import glob
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from bs4 import BeautifulSoup
+import re
+import nltk
+nltk.download("stopwords")   # download list of stopwords (only once; need not run it again)
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+
+from wordcloud import WordCloud, STOPWORDS
+
+def plot_wordcloud(data, sentiment):
+    combined_text = " ".join([review for review in data['train'][sentiment]])
+
+    wc = WordCloud(background_color='white', max_words=50,
+                   stopwords=STOPWORDS.update(['br', 'film', 'movie']))
+
+    plt.imshow(wc.generate(combined_text))
+    plt.axis('off')
+    plt.show()
+
 
 def read_imdb_data(data_dir):
     """
@@ -35,11 +56,56 @@ def read_imdb_data(data_dir):
                     labels[data_type][sentiment].append(sentiment)
     return data, labels
 
-def prepare_data():
-    raise NotImplementedError
+def prepare_data(data):
+    """
+    Prepare training and test sets from IMDb movie reviews.
+    """
+    train_pos_reviews_list = data['train']['pos']
+    train_neg_reviews_list = data['train']['neg']
+    test_pos_reviews_list = data['test']['pos']
+    test_neg_reviews_list = data['test']['neg']
 
-def review_word_tokenizer():
-    raise NotImplementedError
+    train_pos_label_list = len(train_pos_reviews_list) * ['pos']
+    train_neg_label_list = len(train_neg_reviews_list) * ['neg']
+    test_pos_label_list = len(test_pos_reviews_list) * ['pos']
+    test_neg_label_list = len(test_neg_reviews_list) * ['neg']
+
+    train_reviews_list = train_pos_reviews_list + train_neg_reviews_list
+    train_labels_list = train_pos_label_list + train_neg_label_list
+    test_reviews_list = test_pos_reviews_list + test_neg_reviews_list
+    test_labels_list = test_pos_label_list + test_neg_label_list
+
+    data_train, labels_train = shuffle(train_reviews_list, train_labels_list)
+    data_test, labels_test = shuffle(test_reviews_list, test_labels_list)
+
+    # Return a unified training data, test data, training labels, test labels
+    return data_train, data_test, labels_train, labels_test
+
+
+def review_to_words(review):
+    """Convert a raw review string into a sequence of words."""
+    stemmer = PorterStemmer()
+
+    # remove html tags
+    soup = BeautifulSoup(review, "html5lib")
+    text = soup.get_text()
+
+    # convert to lowercase
+    text = text.lower()
+
+    # remove non-letters
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+
+    # tokenize
+    words = text.split()
+
+    # remove stopwords
+    words = [w for w in words if w not in stopwords.words("english")]
+
+    # stemming
+    stemmed = [stemmer.stem(w) for w in words]
+
+    return stemmed
 
 def preprocess_data():
     raise NotImplementedError
@@ -52,6 +118,12 @@ def classify_gboost():
 
 def main():
     data, labels = read_imdb_data('data')
+    print("IMDb reviews: train = {} pos / {} neg, test = {} pos / {} neg".format(
+        len(data['train']['pos']), len(data['train']['neg']),
+        len(data['test']['pos']), len(data['test']['neg'])))
+    plot_wordcloud(data, 'neg')
+    plot_wordcloud(data, 'pos')
+
 
 if __name__ == "__main__":
     main()
